eed1af471bd988ae42346ddc7a41d462a912350c
Guillermoç <guillermodean@hotmail.com>
2021-06-04 18:57:23 +0200

    first commit - table and filter.

---
+++ Classes/Filterdata.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+class Filtrar:
+    def __init__(self):
+        return
+
+    def filtrar_df(self, df):
+        return df
---
+++ app.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import plotly.express as px
+import plotly.graph_objs as go
+from Classes.Filterdata import Filtrar
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+external_scripts = [
+    'https://www.google-analytics.com/analytics.js',
+    {'src': 'https://cdn.polyfill.io/v2/polyfill.min.js'},
+    {
+        'src': 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.10/lodash.core.js',
+        'integrity': 'sha256-Qqd/EfdABZUcAxjOkMi8eGEivtdTkh3b65xCZL4qAQA=',
+        'crossorigin': 'anonymous'
+    }
+]
+external_stylesheets = [
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    {
+        'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+        'rel': 'stylesheet',
+        'integrity': 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+        'crossorigin': 'anonymous'
+    }
+]
+# ----------------------------------------------------------------------------
+# importar data y tratar
+
+df = pd.read_csv('Results_TTM.csv', sep=";", header=0)
+df = df.drop(
+    columns=["Controlling transducer", "Current min (A)", "Current max (A)", "Current min (%)", "Current max (%)",
+             "Current (%)", "Torque rate", "Current (A)"])
+# df = Filterdata.Filtrar(df)
+df = df.drop(
+    columns=["Step status", "Current trend", "Torque rate min", "Torque rate max", "Torque rate trend", "CVILOGIX",
+             "Identifier6", "Identifier7", "Identifier8", "Identifier9", "Identifier10",
+             "Second transducer torque deviation", "Second transducer angle deviation", "Result type", "Pulse counter",
+             "Angle offset", "AO torque rate"])
+df = df.rename(
+    columns={"Result number": "Result_number", "Pset ID": "Pset_ID", "Step ID": "Step_ID", "Torque min": "Torque_min",
+             "Torque max": "Torque_max", "Angle min": "Angle_min", "Angle max": "Angle_max",
+             "Controller serial no.": "Controller_serial_no.", "Pset name": "Pset_name",
+             'Controller name': 'Controller_name', 'Error code': 'Error_code', 'Result_status': 'Result_status'})
+df = df[~df["Pset_name"].str.contains("Poka", na=False)]
+df.astype({'Time result': 'datetime64[ns, US/Eastern]'}).dtypes
+df.reset_index(inplace=True)
+dropdown = list(df['Controller_name'])
+
+
+
+# ----------------------------------------------------------------------------
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
+
+app = dash.Dash(__name__, external_scripts=external_scripts,
+                external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H4(children='ISRI TTM', style={'text-align': 'center'}),
+    dcc.Dropdown(id='dropdown', options=[
+        {'label': str(i), 'value': str(i)} for i in df['Pset_name'].unique()
+    ], multi=True, placeholder='Filter by Pset...'),
+    html.Div(id='table-container')
+])
+
+
+@app.callback(
+    dash.dependencies.Output('table-container', 'children'),
+    [dash.dependencies.Input('dropdown', 'value')])
+def display_table(dropdown_value):
+    if dropdown_value is None:
+        return generate_table(df)
+    else:
+        dff = df[df['Pset_name'].str.contains('|'.join(dropdown_value),na=False)]
+        print (dff.info())
+        return generate_table(dff)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
